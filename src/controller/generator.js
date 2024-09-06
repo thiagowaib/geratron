72a4d90490963dab7b660a3fbb7aa62515d79bd0
Thiago Waib Castello Branco <thiago.waib@dsin.com.br>
2024-09-06 20:23:03 -0300

    Ajustes

--- src/controller/generator.js
+++ src/controller/generator.js
@@ -1,7 +1,6 @@
 /**
  * Elementos do DOM
  */
-const icoCopy        = document.getElementById("icoCopy");
 const icoReload      = document.getElementById("icoReload");
 const inpPassword    = document.getElementById("inpPassword");
 const inpCount       = document.getElementById("inpCount");
@@ -19,17 +18,28 @@ let major   = true;
 let numbers = true; 
 let symbols = true;
 let style   = 1;
-let count   = 10;
+let count   = 16;
 let password = "";
 
 /**
  * Verifica a força da senha, e atribui a cor para
  * a TAG classificadora de acordo
+ * 
+ * Forte :: Possui 3 Alfabetos Diferentes || >32 caracteres
+ * Medio :: Possui 2 Alfabetos Diferentes || >16 caracteres
+ * Fraco :: Possui 1 Alfabeto             || <16 caracteres
  */
 const checkPasswordStrength = () => {
-    if(password.length > 45 || symbols || (numbers && major)){
+    let qtdeAlfabetos = 0;
+    // No Javascript TRUE = 1 e FALSE = 0
+    qtdeAlfabetos += minor
+    qtdeAlfabetos += major
+    qtdeAlfabetos += numbers
+    qtdeAlfabetos += symbols
+
+    if(password.length >= 32 || (password.length >= 16 && qtdeAlfabetos >= 3)) {
         classifier.style.backgroundColor = "#00FF00";
-    } else if (password.length > 8 || numbers || major) {
+    } else if (password.length >= 16 || (password.length >= 8 && qtdeAlfabetos >= 2)) {
         classifier.style.backgroundColor = "#FFFF00";
     } else {
         classifier.style.backgroundColor = "#FF0000";
@@ -40,6 +50,7 @@ const checkPasswordStrength = () => {
  * Gera a senha a partir dos parâmetros definidos
  */
 const generatePassword = () => {
+    // Definição dos alfabetos
     const alphabetMinor   = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     const alphatbetMajor  = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     const alphabetNumbers = ["0","1","2","3","4","5","6","7","8","9"]
@@ -47,6 +58,9 @@ const generatePassword = () => {
 
     let usedAlphabet = [];
 
+    /**
+     * Monta o alfabeto utilizado de acordo com os parametros
+     */
     if(minor) { 
         usedAlphabet = usedAlphabet.concat(usedAlphabet, alphabetMinor); 
     }
@@ -61,17 +75,24 @@ const generatePassword = () => {
     }
 
 
+    /**
+     * Caso os parametros sejam validos, itera pela quantidade de caracteres esperada
+     */
     let pwd = "";
     for(let i = 0; i < count && (usedAlphabet.length > 0); i++) {
+        // Seleciona um elemento do alfabeto disponivel
         let character = usedAlphabet[Math.floor(Math.random() * usedAlphabet.length)]
 
-        while(style == 2 && ["I","l","i","L","1","0","o","O"].includes(character)) {
+        // No modo de acessibilidade, evita escolher caracteres ambíguos (facilmente confundidos)
+        while(style == 2 && ["I","l","1","0","o","O"].includes(character)) {
             character = usedAlphabet[Math.floor(Math.random() * usedAlphabet.length)];
         }
 
+        // Concatena o caractere escolhido
         pwd += character;
     }
 
+    // Define a senha e avalia sua força
     password = pwd;
     inpPassword.value = password;
     checkPasswordStrength();
@@ -127,6 +148,11 @@ for(let i = 0; i<arrChkAlphabet.length; i++){
             symbols = chkAlphabet.checked;
         }
         generatePassword();
+
+        if(password == "") {
+            chkAlphabet.checked = !chkAlphabet.checked;
+            chkAlphabet.dispatchEvent(new Event('input'));
+        }
     }
 }
 
@@ -135,13 +161,6 @@ for(let i = 0; i<arrChkAlphabet.length; i++){
  */
 icoReload.onclick = () => {
     generatePassword();
-}
-
-/**
- * Ao clicar no icone da Copia, copia a senha para a area de transferencia
- */
-icoCopy.onclick = () => {
-    copyPassword();
 }
 
 /**
